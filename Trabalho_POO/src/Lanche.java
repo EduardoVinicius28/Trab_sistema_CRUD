86a14721ddf1e703dbaee73c6b6a00b42f306b75
Eduardo Vinicius <112330596+EduardoVinicius28@users.noreply.github.com>
2025-05-19 19:01:23 -0300

    Update Lanche.java
    Adição de comentarios para maior entendimento

--- Trabalho_POO/src/Lanche.java
+++ Trabalho_POO/src/Lanche.java
@@ -1,8 +1,9 @@
-public class Lanche extends Produto{
+public class Lanche extends Produto{ //Classe Lanche que herda de Produto
      public Lanche(int codigo, String nome, double preco) {
         
         super(codigo, nome, preco);
      }
+     //Implementação do método exibirInformacoes
      @Override
     public String exibirInformacoes() {
         return "Codigo: " + getCodigo() + "\n" + "Produto: " + getProduto() +"\n"+ "Preço: R$:" + getPreco();
